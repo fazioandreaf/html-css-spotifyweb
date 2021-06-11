40eaa29adaa14212652c781fc26193f0ed7a2671
fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-11 17:33:34 +0200

    Aggiunto lo stop

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ function init(){
           buttonLikeIt:false,
           buttonMute:true,
           volume:10,
+          volume_text:'10',
+          counter:1,
           timefinish:100,
           volume_complete:100- this.volume ,
         },
@@ -25,15 +27,27 @@ function init(){
             booleanMute:function(){
                 this.buttonMute=!this.buttonMute;
             },
-            playtime:function(time){
-                tempo=setInterval(function(){
-                    time=time +1;
-                    console.log(time);
-                },1000)
+            playtime:function(){
+                console.log(this.volume);
+               
+                    tempo=setInterval(this.increment,1000);
+                    tempo_testo=setInterval(this.secondsToHms,1000);
+  
+            },
+            increment:function(){
+            if((this.volume>=this.timefinish)?this.stoptime():this.volume++);
+            },
+            secondsToHms:function() {
+                let m = Math.floor(this.volume % 3600 / 60);
+                let s = Math.floor(this.volume % 3600 % 60);
+                let mDisplay = m > 0 ? (m == 1 ? "01:" : 'm') : "";
+                let sDisplay = s > 0 ? s +(s == 1 ? "" : "") : "";
+                this.volume_text= mDisplay + sDisplay; 
             },
             stoptime:function(){
-                clearInterval(tempo)
-            }
+                clearInterval(tempo);
+                clearInterval(tempo_testo);
+            },
         }   
     })
 }
